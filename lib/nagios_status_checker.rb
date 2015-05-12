b6b32c7e978c752211266f56d2e73ca2546f5b69
Sudeep Singh <sudeep.395@gmail.com>
2015-05-12 18:33:26 +0530

    Added code for memory cpu usage

--- lib/nagios_status_checker.rb
+++ lib/nagios_status_checker.rb
@@ -16,11 +16,64 @@ module NagiosStatusChecker
     input_services.each do |serv|
       service_name =  serv.at_xpath("name").text
       if required_services.include? service_name
+
+        performance_data = serv.at_xpath("performance_data").text
+        usage_status = ""
+
+        if (service_name  == '/mysql Disk Usage')
+          usage_status =  get_usage_status(service_name, performance_data, 90, 80)          
+        end 
+
+        if (service_name  == '/ Disk Usage')
+          usage_status =  get_usage_status(service_name, performance_data, 95, 90)          
+        end 
+
+        if (service_name  == 'Memory Usage')
+          usage_status =  get_usage_status(service_name, performance_data, 95, 90)   
+        end 
+
+        if (service_name  == 'Swap Usage')
+          usage_status =  get_usage_status(service_name, performance_data, 80, 90)          
+        end 
+
+        if (service_name  == 'CPU Usage')
+          usage_status =  get_usage_status(service_name, performance_data, 95, 90)          
+        end 
+
+        if (usage_status == "red" || usage_status == "yellow")
+          return usage_status
+        end
+
         return "red" unless serv.at_xpath("current_state").try(:text) and serv.at_xpath("current_state").text.to_i.zero?
+
       end
     end
     return "green"
   end
+
+
+  def self.get_usage_status(service_name, performance_data, critical_limit, warning_limit)
+
+    percent_used = 0
+
+    if (service_name  == 'CPU Usage')
+      percent_used = performance_data.split("=")[1].split(';')[0][0...-1].to_i
+    else
+      consumed_memory = performance_data.split("=")[1].split(';')[0][0...-2].to_f
+      total_memory = performance_data.split("=")[1].split(';')[-1].to_f
+      percent_used = ((consumed_memory/total_memory)*100).to_i             
+    end
+
+      if (percent_used >= critical_limit)
+        return "red"
+      elsif (percent_used >= warning_limit)
+        return "yellow"
+      else
+        return "green"
+      end
+
+  end  
+
 
   def self.get_memory_status(host_list)
    tbbClusterCPU, tbbClusterMemory, tbbClusterSWAP, tbbClusterDisk = 0, 0, 0, 0
