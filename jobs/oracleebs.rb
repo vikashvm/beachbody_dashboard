c9341dd1625b4729474c9f5628cbc9e1ee7e3e76
vikash vikram <vikashvikram@gmail.com>
2015-05-12 16:56:39 +0530

    refactoring oracle ebs

--- jobs/oracleebs.rb
+++ jobs/oracleebs.rb
@@ -4,28 +4,12 @@ SCHEDULER.every CONFIG['interval'], :first_in => CONFIG['start_time'] do
 	threads = []
 	flags = []
 
-	threads << Thread.new do
-		services = NagiosStatusChecker.get_services(CONFIG["oracle_esb_host_1"])
-		req_services = CONFIG["oracle_esb_services_1"]
-		flags << NagiosStatusChecker.check_status(services, req_services)
-	end
-
-	threads << Thread.new do
-		services = NagiosStatusChecker.get_services(CONFIG["oracle_esb_host_2"])
-		req_services = CONFIG["oracle_esb_services_1"]
-		flags << NagiosStatusChecker.check_status(services, req_services)
-	end
-
-	threads << Thread.new do
-		services = NagiosStatusChecker.get_services(CONFIG["oracle_esb_host_3"])
-		req_services = CONFIG["oracle_esb_services_1"]
-		flags << NagiosStatusChecker.check_status(services, req_services)
-	end
-
-	threads << Thread.new do
-		services = NagiosStatusChecker.get_services(CONFIG["oracle_esb_host_4"])
-		req_services = CONFIG["oracle_esb_services_1"]
-		flags << NagiosStatusChecker.check_status(services, req_services)
+	CONFIG['oracle_ebs_hosts'].each do |host|
+		threads << Thread.new do
+			services = NagiosStatusChecker.get_services(host)
+			req_services = CONFIG["oracle_ebs_services_1"]
+			flags << NagiosStatusChecker.check_status(services, req_services)
+		end
 	end
 
   # OracleEBS splunk data
